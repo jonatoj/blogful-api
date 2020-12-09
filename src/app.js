33e4f9d6a7ba361a613ff64d36141737df8e7385
Jonathan Toj <tojjonathan@yahoo.com>
2020-12-09 11:20:07 -0500

    Updated Files

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ const morgan = require("morgan");
 const cors = require("cors");
 const helmet = require("helmet");
 const { NODE_ENV } = require("./config");
+const ArticlesService = require("./articles/articles-service");
 const app = express();
 
 const morganOption = NODE_ENV === "production" ? "tiny" : "common";
@@ -11,6 +12,14 @@ const morganOption = NODE_ENV === "production" ? "tiny" : "common";
 app.use(morgan(morganOption));
 app.use(helmet());
 app.use(cors());
+app.get("/articles", (req, res, next) => {
+  const knexInstance = req.app.get("db");
+  ArticlesService.getAllArticles(knexInstance)
+    .then((articles) => {
+      res.json(articles);
+    })
+    .catch(next);
+});
 
 app.get("/", (req, res) => {
   res.send("Hello World");
